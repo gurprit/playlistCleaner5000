c07fe64d7d3738beb73bb8dd561a55c16488abfa
Gurprit <gurprit.sahota@gmail.com>
2025-03-30 20:12:05 +0000

    poop

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+app.get("/", (req, res) => {
+    res.send("Spotify API is running! Go to <a href='/login'>Login with Spotify</a>");
+});
+
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
